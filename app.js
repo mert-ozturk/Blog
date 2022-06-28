a554a20ba4d0192e51ef13970f47042570ec5cfd
mert-ozturk <38337566+mert-ozturk@users.noreply.github.com>
2022-06-28 07:55:24 +0300

    deneme

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ('express')
+const path = require('path')
+
+const app = express()
+
+//MIDDLEWARES
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    //göndermek istediğimiz klasörü yönlendirme
+    //__dirname proje klasörü olduğu yer 
+    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+       
+})
+
+
+const port = 3000
+app.listen(port,()=> {
+    console.log(`sunucu ${port}`)
+})  
